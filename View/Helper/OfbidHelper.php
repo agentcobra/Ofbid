7687b2b0f31447d613c5b8fbdefa4b039d81f047
Joey jan <overflow636@gmail.com>
2012-01-25 14:52:54 -0600

    added imageButton to helper to use the pretty browserid login buttons

--- View/Helper/OfbidHelper.php
+++ View/Helper/OfbidHelper.php
@@ -8,13 +8,28 @@ class OfbidHelper extends AppHelper
 
 	function loginButton($text='Login with BrowserID')
 	{
-		$out = '';
-		$out .= $this->Html->script('https://browserid.org/include.js');
+		$out = $this->Html->tag('button', $text, array('onClick'=>'ofbid_login()'));
+		
+		return $this->__head().$out.$this->__foot();
+	}
+	
+	function imageButton($color='green')
+	{
+		$out = $this->Html->image("/ofbid/img/sign_in_$color.png", array('onClick'=>'ofbid_login()', 'style'=>'cursor:pointer'));
+		
+		return $this->__head().$out.$this->__foot();
+	}
+	
+	function __head()
+	{
+		$out  = $this->Html->script('https://browserid.org/include.js');
 		$out .= $this->Html->script('http://code.jquery.com/jquery-1.7.1.min.js');
-		
-		$out .= $this->Html->tag('button', $text, array('onClick'=>'ofbid_login()'));
-		
-		$out .= '<script>
+		return $out;
+	}
+	
+	function __foot()
+	{
+		return '<script>
 			function ofbid_login() {
 				navigator.id.get(function(assertion) {
 					if (assertion) {
@@ -33,7 +48,5 @@ class OfbidHelper extends AppHelper
 				});
 			}
 			</script>';
-		
-		return $out;
 	}
 }
