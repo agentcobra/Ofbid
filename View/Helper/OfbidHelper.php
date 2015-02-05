773a699047d265d00eacad94c85aee139e6a2b04
agentcobra <agentcobra@free.fr>
2015-02-05 19:20:12 +0100

    add more persona icons more options for the helper

--- View/Helper/OfbidHelper.php
+++ View/Helper/OfbidHelper.php
@@ -1,41 +1,37 @@
 <?php
 
-class OfbidHelper extends AppHelper
-{
-	public $helpers = array(
-		'Html'
-	);
+class OfbidHelper extends AppHelper {
 
-	function loginButton($text='Login with BrowserID', $tag='button', $options=array())
-	{
-		$options = array_merge($options, array('onClick'=>'ofbid_login()'));
-		$out = $this->Html->tag($tag, $text, $options);
+  public $helpers = array(
+      'Html'
+  );
 
-		return $this->__head().$out.$this->__foot();
-	}
+  function loginButton($text = 'Login with BrowserID', $tag = 'button', $options = array()) {
+    $options = array_merge($options, array('onClick' => 'ofbid_login()'));
+    $out = $this->Html->tag($tag, $text, $options);
 
-	function imageButton($color='green')
-	{
-		$out = $this->Html->image("/ofbid/img/sign_in_$color.png", array('onClick'=>'ofbid_login()', 'style'=>'cursor:pointer'));
+    return $this->__head() . $out . $this->__foot();
+  }
 
-		return $this->__head().$out.$this->__foot();
-	}
+  function imageButton($color = 'green', $type = '') {
+    $out = $this->Html->image("/ofbid/img/" . $type . "sign_in_$color.png", array('onClick' => 'ofbid_login()', 'style' => 'cursor:pointer'));
 
-	function __head()
-	{
-		$out  = $this->Html->script('https://login.persona.org/include.js',['inline'=>false]);
-		$out .= $this->Html->script('http://code.jquery.com/jquery-1.7.1.min.js',['inline'=>false]);
-		return $out;
-	}
+    return $this->__head() . $out . $this->__foot();
+  }
 
-	function __foot()
-	{
-		$persona = 'function ofbid_login() {
+  function __head() {
+    $out = $this->Html->script('https://login.persona.org/include.js', ['inline' => false]);
+    //$out .= $this->Html->script('http://code.jquery.com/jquery-1.7.1.min.js');
+    return $out;
+  }
+
+  function __foot() {
+    $persona = 'function ofbid_login() {
 				navigator.id.get(function(assertion) {
 					if (assertion) {
 						$.ajax({
 							type: \'POST\',
-							url: \'/ofbid/authenticate\',
+							url: \'ofbid/authenticate\',
 							data: { assertion: assertion },
 							success: function(res, status, xhr) {
 									window.location.href = res;
@@ -47,6 +43,7 @@ class OfbidHelper extends AppHelper
 					}
 				});
 			}';
-		return $this->Html->scriptBlock($persona, ['inline' => false]);
-	}
+    return $this->Html->scriptBlock($persona, ['inline' => false]);
+  }
+
 }
